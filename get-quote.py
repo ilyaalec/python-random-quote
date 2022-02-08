c7d15bb35e37d24c361e8debeb14c0ffa3495593
Lab Admin <ilyapasion@yahoo.com>
2022-02-08 00:32:59 -0400

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,6 @@
-def primary():
-  print("Keep it logically awesome.")
+f = open("quotes.txt")
+quotes = f.readlines()
+f.close()
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+print(quotes[13])
 
-  #print(quotes)
-
-if __name__== "__main__":
-  primary()
